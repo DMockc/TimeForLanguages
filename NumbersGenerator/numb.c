73a9a0ef9c39799df32cbb3da4a61583814803e7
DMockc <118026464+DMockc@users.noreply.github.com>
2023-01-08 20:33:36 +0100

    Update numb.c

--- NumbersGenerator/numb.c
+++ NumbersGenerator/numb.c
@@ -4,12 +4,12 @@
 int main(){
     long start = time(NULL);
     int count = 0;
-    while(count<=100000){
+    while(count <= 100000){
         printf("%d\n", count);
         count++;
     }
     long end = time(NULL);
-    int time = (end-start); 
+    int time = (end - start); 
 
     printf("\nCurrent time: %d seconds.", time);
     return 0;
